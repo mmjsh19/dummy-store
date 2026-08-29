9e999685b890e09cbd99f82e997828ab38379391
RezaEbrahimi81 <r3z4.3br4h1m1@gmail.com>
2026-08-29 09:18:31 +0330

    chore: initial project setup with Tailwind CLI

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./pages/**/*.html",
+    "./src/js/**/*.js",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
